9af030f76e6cf29efd8ff820f7d67e96a37d4249
ATLAS4096 <atlas4096@gmail.com>
2017-05-28 09:57:54 -0700

    Add files via upload

---
+++ test-countdown-1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# This test implements a count up/down time.
+#
+
+import json
+import datetime
+
+daysInOffice = datetime.datetime.now() - datetime.datetime(year=2017, month=1, day=20)
+daysLeftInTerm = datetime.datetime(year=2021, month=1, day=20) - datetime.datetime.now()
+
+print 'Days in office: ' + str(daysInOffice.days)
+print 'Days remaining in term: ' + str(daysLeftInTerm.days)
+
+inJSON = json.dumps({'days_in_office': daysInOffice.days, 'days_remaining_in_term': daysLeftInTerm.days}, 
+    sort_keys=True, 
+    indent=4, 
+    separators=(',', ' : '))
+
+fileName = 'countdown.json'
+file = open(fileName, "w")
+file.write(inJSON)
+file.close()
+
+
