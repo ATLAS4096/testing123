0ad46ae3350940c20d1e469ffb20128e4e3af28c
Russ <russ@thingtone.net>
2017-05-28 10:21:53 -0700

    Added quiet flag.

--- test-countdown-1.py
+++ test-countdown-1.py
@@ -8,11 +8,14 @@
 import json
 import datetime
 
+quiet = False
+
 daysInOffice = datetime.datetime.now() - datetime.datetime(year=2017, month=1, day=20)
 daysLeftInTerm = datetime.datetime(year=2021, month=1, day=20) - datetime.datetime.now()
 
-print 'Days in office: ' + str(daysInOffice.days)
-print 'Days remaining in term: ' + str(daysLeftInTerm.days)
+if not quiet:
+    print 'Days in office: ' + str(daysInOffice.days)
+    print 'Days remaining in term: ' + str(daysLeftInTerm.days)
 
 inJSON = json.dumps({'days_in_office': daysInOffice.days, 'days_remaining_in_term': daysLeftInTerm.days}, 
     sort_keys=True, 
